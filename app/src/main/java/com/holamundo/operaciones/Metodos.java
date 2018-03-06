72d5a072beee80b1e3dfe96a9869321fa092dace
Android Sala 15 <josechavez1796@gmail.com>
2018-03-05 20:35:51 -0500

    primer commit

---
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -0,0 +1,23 @@
+package com.holamundo.operaciones;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar(double n1, double n2){
+        return n1+n2;
+    }
+
+    public static double restar(double n1, double n2){
+        return n1-n2;
+    }
+
+    public static double multiplicar(double n1, double n2){return n1*n2;
+    }
+
+    public static double dividir(double n1, double n2){
+        return n1/n2;
+    }
+}
